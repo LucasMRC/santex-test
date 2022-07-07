effecdb23c75b894a1ab0a1c4109f3ba84444d1d
lucas <lucas.reynacordoba22@gmail.com>
2022-07-07 20:47:09 -0300

    Total items: Adds data to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ import { useEffect, useRef } from 'react';
 import {
 	HeaderContainer,
 	HeaderImage,
-	HeaderSubtotal
+	HeaderOrderInfo,
+	HeaderSubtotal,
+	HeaderTotalItems
 } from './styled';
 
 // Hooks
@@ -17,14 +19,20 @@ import { formatPrice } from '../helpers/index';
 export function Header() {
 	const { order, clearCart } = useOrder();
 	const [ subtotal, setSubtotal ] = useStateWithStorage<number>('subtotal', 0);
+	const [ totalItems, setTotalItems ] = useStateWithStorage<number>('totalItems', 0);
 	const subTotalFromOrder = useRef<number>(order.subtotal);
 
 	useEffect(() => {
 		if (order.subtotal !== subTotalFromOrder.current) {
+			const quantity = order.products.reduce((acc, product) => {
+				acc += product.quantity;
+				return acc;
+			}, 0);
 			setSubtotal(order.subtotal);
+			setTotalItems(quantity);
 			subTotalFromOrder.current = order.subtotal;
 		}
-	}, [ order.subtotal, setSubtotal ]);
+	}, [order.products, order.subtotal, setSubtotal, setTotalItems]);
 
 	return (
 		<HeaderContainer>
@@ -32,9 +40,14 @@ export function Header() {
 				src="https://santex.wpengine.com/wp-content/uploads/2019/02/logo-santex@3x.png"
 				alt="logo"
 			/>
-			<HeaderSubtotal>
-				{formatPrice(subtotal)}
-			</HeaderSubtotal>
+			<HeaderOrderInfo>
+				<HeaderSubtotal>
+					{!!totalItems ? formatPrice(subtotal) : '\xa0'}
+				</HeaderSubtotal>
+				<HeaderTotalItems>
+					{`${totalItems} item${totalItems === 1 ? '' : 's'} in chart`}
+				</HeaderTotalItems>
+			</HeaderOrderInfo>
 			<button
 				style={{ position: 'absolute', right: 0 }}
 				onClick={clearCart}
